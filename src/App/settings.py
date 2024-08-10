301445fe4c75010b978705efe965d5a4027b9409
MuhammadBinSikandar <m.bin.sikandar@gmail.com>
2024-08-10 16:41:00 +0500

    Setup the environment variables

--- src/App/settings.py
+++ src/App/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0cuts*dwcjzh(4fuj3)riwp9h6*0%9hd48x54$qq&^8j(mm#@o'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower == "true"
+DEBUG = config("DJANGO_DEBUG", cast= bool)
+
+print("DEBUG :", DEBUG, type(DEBUG) )
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas_platform.prod.railway.app
